e5344f1c801aad1399f141f76a407b81f6d99fbf
molllllly <460984119@qq.com>
2024-09-26 00:03:02 +0200

    改变关卡

--- scripts/exit.gd
+++ scripts/exit.gd
@@ -1,9 +1,11 @@
-@tool
 extends Area2D
 
-@export_file("*.tscn") var path
+#@export_file("*.tscn") var path
+
+var path = "res://scenes/word/game02.tscn";
+
+var scene = preload("res://scenes/word/game02.tscn").instantiate()
 
 func _on_body_entered(body: Node2D) -> void:
+	print("111")
 	Globals.go_to_world(path)
-	#pass # Replace with function body.
-	
--- scripts/global.gd
+++ scripts/global.gd
@@ -2,6 +2,7 @@ extends Node
  
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+
 func reload_word():
 	animation_player.play_backwards("fade-in");
 	await(animation_player.animation_finished) 
@@ -10,8 +11,9 @@ func reload_word():
 
 
 
-func _animate_transition_to(path):
-	animation_player.play_backwards("fade-in")
-	await(animation_player.animation_finished)
-	get_tree().change_scene(path)
-	animation_player.play("fade-in")
+func go_to_world(path):
+	#animation_player.play_backwards("fade-in")
+	#await(animation_player.animation_finished)
+	get_tree().change_scene_to_file(path)
+	#get_tree().change_scene_to(path)
+	#animation_player.play("fade-in")
