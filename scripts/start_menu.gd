b2f21c1e8bf23740eb1905ac2b88538dc1ec5c57
molllllly <460984119@qq.com>
2024-10-17 16:51:47 +0200

    change sprite

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -2,10 +2,9 @@ extends Button
 
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/game_1.tscn")
 	
 	
-
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
